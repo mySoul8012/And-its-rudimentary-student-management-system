5bc8921d6dd764599338eba24633b7b3e8c29aba
ming <mingming@mingming.email>
2018-12-13 01:08:14 +0800

    增加文件

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
+import java.sql.*;
 public class Main {
 
     public static void main(String[] args) {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test","test","123456");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from test");
+            while(resultSet.next()){
+                System.out.println(resultSet.getString(1));
+            }
         } catch (ClassNotFoundException e) {
+            System.out.println("he");
+        } catch (SQLException e){
+            System.out.println("hhh");
         }
     }
 }
