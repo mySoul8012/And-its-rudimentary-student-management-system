935f2260d79787f68b3b130fc88feb929df37491
ming <mingming@mingming.email>
2019-01-05 17:31:51 +0800

    增加文件

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,12 @@
-import com.ming.MiddleLayer.*;
-import com.ming.ServiceLayer.UserLogin;
-import com.ming.entity.CourseSelection;
-import com.ming.entity.Curriculum;
-import com.ming.entity.Student;
-import com.ming.entity.User;
-import com.ming.tools.Message;
 import com.ming.view.mainUi.MainUi;
 import com.ming.web.WebServer;
-import com.ming.web.WebThread;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         // 0780506004
+        MainUi mainUi = new MainUi();
         WebServer webServer = new WebServer();
         //MainUi mainUi = new MainUi();
     }
